4d8dad15e38b046792c158c8c4d93b62cac3e35e
Túlio <tuliovictorp@hotmail.com>
2024-03-07 22:49:30 -0300

    get employees

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ def insertEmployee(name, email, role, salary):
     dicEemployee = {"name": name, "email": email, "role": role, "salary": salary}
     employeesColumns = ', '.join(dicEemployee.keys())
 
-    
-    print(employeesColumns)
-    # query = f'INSERT INTO employees ({employeesColumns[0],}{employeesColumns[1],}{employeesColumns[2],}{employeesColumns[3],}) VALUES("{employeeValues[0]}","{employeeValues[1]}","{employeeValues[2]}","{employeeValues[3]}" )'
     query = f'INSERT INTO employees ({employeesColumns}) VALUES("{dicEemployee['name']}","{dicEemployee['email']}","{dicEemployee['role']}","{dicEemployee['salary']}")'
     cursor.execute(query)
     connection.commit()
@@ -17,4 +14,20 @@ def insertEmployee(name, email, role, salary):
     closeConn(connection, cursor)
     print("registrado")
 
-insertEmployee("refac", "refac@email.com", "re", 4000)
+def getEmployees(table):
+    connection, cursor = openConn()
+    query = f'SELECT * FROM {table}'
+
+    cursor.execute(query)
+    result = cursor.fetchall() 
+
+    def readEmployees():
+        list(map(lambda emp: print(emp), result))
+        
+    readEmployees()
+    closeConn(connection, cursor)
+
+
+# insertEmployee("refac", "refac@email.com", "re", 4000)
+        
+getEmployees('employees')
